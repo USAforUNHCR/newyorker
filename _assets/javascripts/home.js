e153b2107189938d0e232093c7dd878d67173802
Chris Wallwork <chris@unrefgugees.org>
2015-12-18 16:15:17 -0500

    added basic test functionality

---
+++ _assets/javascripts/home.js
@@ -0,0 +1,60 @@
+var content = {
+  intro: {
+    title: "This is the intro title",
+    content: 'Here is some intro content',
+  },
+  clouds: { title: "This is the clouds title",
+            content: "This is the clouds content"
+          }
+}
+
+$(document).ready(function(){
+  scrollBottom();
+  cloudListener();
+  bodyListener();
+})
+
+
+function scrollBottom(){
+  $('body').animate({ 
+     scrollTop: $(document).height()},
+     1400,
+    'linear', function(){
+    createModal('intro');
+    }
+  );
+}
+
+function createModal(item){
+  var contentModal = $('#content-modal');
+  if(content[item]){
+    contentModal.find('.modal-title').html(content[item].title);
+    contentModal.find('.body-content').html(content[item].content);
+  }
+  // placeholder until all content is added
+  else {
+    contentModal.find('.modal-title').html('This is the ' + item + ' title' );
+    contentModal.find('.body-content').html('This is the ' + item + ' content');
+  }
+  contentModal.modal('show');
+}
+
+function introListener(){
+
+}
+
+function cloudListener(){
+  $('#clouds').click(function(event){
+    event.preventDefault();
+    createModal('clouds');
+  });
+}
+
+function bodyListener(){
+  $('.flex-body').click(function(event){
+    var target = event.target.closest('.flex-item');
+    if(target){
+      createModal(target.id);
+    }
+  })
+}
