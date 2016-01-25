63bbc86ef67a69e0c043d447ecedb46826626f05
Chris Wallwork <chris@unrefgugees.org>
2016-01-25 14:05:11 -0500

    fixed for safari

--- _assets/javascripts/home.js
+++ _assets/javascripts/home.js
@@ -1,3 +1,21 @@
+'use-strict';
+
+var gw = new Groundwork({
+  'api_url': 'https://api.thegroundwork.com',
+  'oauth_client_id': 'pub-un-test.forced-to-flee-int-D.TZUr78VxrMoATcZhlXyr41zUx855kiPID0DKhY5GCSA8.3dajo6b6OfyHjS4efuyJ7RvkRbo7Zi8k8HmVWFw'
+});
+
+function sendData(data){
+  data.tags.send_email = 0;
+  gw.supporters.create(data)
+  .then(function(res){
+    console.log(res);
+  })
+  .catch(function(res){
+    console.log(res);
+  });
+};
+
 $(document).ready(function(){
   introModal();
   bodyListener();
@@ -11,6 +29,7 @@ function introModal(){
 
 function createModal(item){
   var contentModal = $('#content-modal');
+  resetModal(contentModal);
   if(content[item]){
     contentModal.find('.modal-title').html(content[item].title);
     contentModal.find('.body-content').html(content[item].content);
@@ -21,6 +40,13 @@ function createModal(item){
   contentModal.modal('show');
 }
 
+function resetModal(modal){
+  var form = modal.find('.modal-form');
+  modal.find('.social-icons').hide();
+  form.find("input").val("");
+  form.show();
+}
+
 function introListener(){
   $('.intro').click(function(event){
     event.preventDefault();
@@ -28,22 +54,36 @@ function introListener(){
   });
 }
 
-function cloudListener(){
-  $('#clouds').click(function(event){
-    event.preventDefault();
-    createModal('clouds');
-  });
-}
-
 function bodyListener(){
   $('.flex-container').click(function(event){
-    var target = event.target.closest('.flex-item');
+    var target = $(event.target).closest('.flex-item');
     if(target){
-      createModal(target.id);
+      createModal(target.attr('id'));
     }
   })
 }
 
 function formListener(){
-  var form = $('')
+  var form = $('.signup');
+  form.submit(function(event){
+    event.preventDefault();
+    var data = {};
+    data = separateNames(form,data);
+    data.tags = {};
+    data.email = form.find('[name="email"]').val();
+    data.tags.email1 = form.find('[name="email1"]').val();
+    data.source = "New Yorker " + $('.modal-title').html();
+
+    console.log(data);
+    $('.modal-form').hide();
+    $('.social-icons').show();
+  });
 }
+
+function separateNames(form, data){
+  var names = form.find('[name="name"]').val().split(" ");
+  data.givenName = names[0];
+  names[1] ? data.familyName = names[1] : null;
+  return data; 
+}
+
